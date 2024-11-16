0bbcb46f18dbdaa98d72c2648a3d04e7db013045
symao <2564800708@qq.com>
2024-11-16 14:19:57 +0800

    init

---
+++ src/main/java/com/llm/llm_knowledge/LlmKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.llm.llm_knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmKnowledgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llm/llm_knowledge/LlmKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llm.llm_knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
