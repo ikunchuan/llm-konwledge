1fd7f97a24f41d83fae9a52fd69a9f51308b4e1d
Anny <3350265483@qq.com>
2024-11-29 11:37:28 +0800

    userinfo实体更新

--- src/main/java/com/llm/llm_knowledge/entity/UserInfo.java
+++ src/main/java/com/llm/llm_knowledge/entity/UserInfo.java
@@ -1,4 +1,28 @@
 package com.llm.llm_knowledge.entity;
 
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("user_info")
 public class UserInfo {
+    @TableId(type = IdType.AUTO)
+    private Integer userId;//用户id
+    private String userName;//用户
+    private String userPassword;
+    private String userPhone;
+    private String userEmail;
+    private String userProfilePicture;
+    private String userBio;
+    //字段添加填充内容
+    @TableField(fill= FieldFill.INSERT)
+    private Date createdTime;
+    @TableField(fill= FieldFill.INSERT_UPDATE)
+    private Date updatedTime;
 }
--- src/main/java/com/llm/llm_knowledge/mapper/UserInfoMapper.java
+++ src/main/java/com/llm/llm_knowledge/mapper/UserInfoMapper.java
@@ -1,4 +1,10 @@
 package com.llm.llm_knowledge.mapper;
 
-public class UserInfoMapper {
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.llm.llm_knowledge.entity.UserInfo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+
 }
