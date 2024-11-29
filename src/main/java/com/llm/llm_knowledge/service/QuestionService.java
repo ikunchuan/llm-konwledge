a1c47e687ca8dd08ab4dbe2d8d0974f4f59a6793
HengXiaoYu <1795338344@qq.com>
2024-11-29 19:10:58 +0800

    测试和配置

--- src/main/java/com/llm/llm_knowledge/service/QuestionService.java
+++ src/main/java/com/llm/llm_knowledge/service/QuestionService.java
@@ -2,21 +2,34 @@ package com.llm.llm_knowledge.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.llm.llm_knowledge.entity.Question;
-import com.llm.llm_knowledge.mapper.QuestionMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public interface QuestionService {
-//    分页查询
-    Page<Question> findAllPageQuestion();
-//
-//    List<Question> findAllQuestionById();
+    //插入题目
+    Integer insertQuestion(Question question);
 
+    //根据id删除
+    Integer deleteQuestionById(Question question);
+
+    //根据id批量删除
+    Integer deleteQuestionByIds(List<Question> ids);
+
+    //条件删除
+    Integer criteriaDeleteQuestion(Question question);
 
     //更新题目
-    Integer updateQuestionById();
+    Integer updateQuestionById(Question question);
 
+    //根据id查询
+    List<Question> findQuestionById(Question question);
 
-    List<Question> findAllQuestion();
+    //根据id批量查询
+    List<Question> findQuestionByIds(List<Question> ids);
+
+    //条件查询
+    List<Question> criteriaFindQuestion(Question question);
+
+    //分页查询全部记录
+    Page<Question> findAllPageQuestion(Question question);
 }
--- src/main/java/com/llm/llm_knowledge/service/QuestionServiceImpl.java
+++ src/main/java/com/llm/llm_knowledge/service/QuestionServiceImpl.java
@@ -3,29 +3,93 @@ package com.llm.llm_knowledge.service;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.llm.llm_knowledge.entity.Question;
 import com.llm.llm_knowledge.mapper.QuestionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public class QuestionServiceImpl implements QuestionService{
-    private final QuestionMapper questionMapper;
+@Service
+@Transactional
+public class QuestionServiceImpl implements QuestionService {
 
-    public QuestionServiceImpl(QuestionMapper questionMapper) {
-        this.questionMapper = questionMapper;
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    //插入题目
+    @Override
+    public Integer insertQuestion(Question question) {
+        int result = questionMapper.insert(question);//会自动插入ID
+        System.out.println(result);
+        System.out.println(question);
+        return result;
     }
 
+    //根据id删除
     @Override
-    public Page<Question> findAllPageQuestion() {
+    public Integer deleteQuestionById(Question question) {
+        Integer i = questionMapper.deleteById(question);
+        System.out.println(i);
+        return i;
+    }
+
+    //根据id批量删除
+    @Override
+    public Integer deleteQuestionByIds(List<Question> ids) {
+        List<Integer> ids_ = new ArrayList<>();
+        for (Question question : ids) {
+            Integer questionId = question.getQuestionId();
+            ids_.add(questionId);
+        }
+        int i = questionMapper.deleteByIds(ids_);
+        System.out.println(i);
+        return i;
+    }
+
+    //条件删除
+    @Override
+    public Integer criteriaDeleteQuestion(Question question) {
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("question_title","两数之和1");
+        questionMapper.deleteByMap(map);
         return null;
     }
 
+    //更新题目
     @Override
-    public Integer updateQuestionById() {
+    public Integer updateQuestionById(Question question) {
         return null;
     }
 
+    //根据id查询
     @Override
-    public List<Question> findAllQuestion() {
-        return questionMapper.selectList(null);
+    public List<Question> findQuestionById(Question question) {
+        return null;
     }
 
+    //根据id批量查询
+    @Override
+    public List<Question> findQuestionByIds(List<Question> ids) {
+        List<Integer> ids_ = new ArrayList<>();
+        for (Question question : ids) {
+            Integer questionId = question.getQuestionId();
+            ids_.add(questionId);
+        }
+        List<Question> questions = questionMapper.selectByIds(ids_);
+        questions.
+        return i;
+        return null;
+    }
+
+    //条件查询
+    @Override
+    public List<Question> criteriaFindQuestion(Question question) {
+        return null;
+    }
+
+    //分页查询全部记录
+    @Override
+    public Page<Question> findAllPageQuestion(Question question) {
+        return null;
+    }
 }
