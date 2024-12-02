8f92e0dd733281a0118e08442ff95a6af82cb5f4
HengXiaoYu <1795338344@qq.com>
2024-12-02 15:11:31 +0800

    cat

--- src/main/java/com/llm/llm_knowledge/controller/categoryController.java
+++ src/main/java/com/llm/llm_knowledge/controller/categoryController.java
@@ -2,12 +2,8 @@ package com.llm.llm_knowledge.controller;
 
 import com.llm.llm_knowledge.entity.Category;
 import com.llm.llm_knowledge.service.CategoryService;
-import com.llm.llm_knowledge.service.QuestionService;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.management.MXBean;
 
 @RestController
 @CrossOrigin
@@ -18,10 +14,25 @@ public class categoryController {
 
     //插入
     @PostMapping("v1")
-    public Integer addCategory(Category category){
+    public Integer addCategory(@RequestBody Category category){
         return categoryService.addCategory(category);
     }
 
-    @DeleteMapping("v1")
+    //删除
+    @DeleteMapping("v1/{id}")
+    public Integer deleteCategoryById(@PathVariable Integer id){
+        return categoryService.deleteCategoryById(id);
+    }
 
+    //更新
+    @PutMapping("v1")
+    public Integer updateCategory(@RequestBody Category category){
+        return categoryService.updateCategory(category);
+    }
+
+    //查询
+    @GetMapping("v1")
+    public Category findCategoryById(@RequestParam Integer id){
+        return categoryService.findCategoryById(id);
+    }
 }
