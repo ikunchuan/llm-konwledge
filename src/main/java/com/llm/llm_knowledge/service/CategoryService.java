aedb3eb33f6f474d74dd756a40a14dc7b6d5d26e
weishenying <1724579910@>
2024-12-03 17:48:28 +0800

    修改了category，但是没有实现，应该是变量名的问题

--- src/main/java/com/llm/llm_knowledge/service/CategoryService.java
+++ src/main/java/com/llm/llm_knowledge/service/CategoryService.java
@@ -6,6 +6,13 @@ import com.llm.llm_knowledge.entity.Category;
 import java.util.List;
 
 public interface CategoryService {
+
+    // 获取所有父类（parentId == 0）
+    List<Category> getParentCategories();
+
+    // 根据父类 ID 获取对应的子类
+    List<Category> getSubCategoriesByParentId(Integer parentId);
+
     Integer addCategory(Category category);
     Integer deleteCategoryById(Integer id);
     Integer deleteCategoryByIds(List<Integer> ids);
