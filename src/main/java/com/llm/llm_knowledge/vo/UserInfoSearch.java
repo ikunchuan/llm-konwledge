e5a0a058c880cedad2694b13b7edc0d69adf055a
myc <3422477587@qq.com>
2024-12-08 18:35:21 +0800

    完善

---
+++ src/main/java/com/llm/llm_knowledge/vo/UserInfoSearch.java
@@ -0,0 +1,15 @@
+package com.llm.llm_knowledge.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoSearch {
+    
+    private Integer userSex;
+    private String userName;
+    
+}
