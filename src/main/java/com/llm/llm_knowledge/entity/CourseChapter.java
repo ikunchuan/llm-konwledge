31ac2b56ab497f3efb8b834ae27be3a1c7d942ad
myc <3422477587@qq.com>
2024-12-14 14:33:57 +0800

    课程章节增删改

--- src/main/java/com/llm/llm_knowledge/entity/CourseChapter.java
+++ src/main/java/com/llm/llm_knowledge/entity/CourseChapter.java
@@ -1,8 +1,6 @@
 package com.llm.llm_knowledge.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +12,7 @@ import java.util.Date;
 @NoArgsConstructor
 @TableName("course_chapter")
 public class CourseChapter {
+    @TableId(type= IdType.AUTO)
     private Integer chapterId;
     private String chapterName;
     private Integer courseId;
