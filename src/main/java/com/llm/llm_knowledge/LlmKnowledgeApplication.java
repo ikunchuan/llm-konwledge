cd5a017d159b54b84e16c5ac6328722daf52e729
myc <3422477587@qq.com>
2024-12-15 21:52:50 +0800

    dddd

--- src/main/java/com/llm/llm_knowledge/LlmKnowledgeApplication.java
+++ src/main/java/com/llm/llm_knowledge/LlmKnowledgeApplication.java
@@ -3,12 +3,13 @@ package com.llm.llm_knowledge;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableAsync
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.llm.llm_knowledge.repository")
+@MapperScan(basePackages = "com.llm.llm_knowledge.mapper")
 @MapperScan(basePackages = "com.llm.llm_knowledge.mapper")
 public class LlmKnowledgeApplication {
 
